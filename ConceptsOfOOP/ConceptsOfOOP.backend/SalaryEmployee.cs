a4af9f732f09ea945d80d2c8269d7e5117c2ed8f
JERONIMORAMIREZRESTREPO <jeronimoramirez1159288@correo.itm.edu.co>
2026-02-19 20:26:54 -0500

    add salary empployee class :)

---
+++ ConceptsOfOOP/ConceptsOfOOP.backend/SalaryEmployee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConceptsOfOPP.Backend;
+
+public class SalaryEmployee : Employee
+{
+    //fields 
+    private decimal _salary;
+
+    //constructor
+    public SalaryEmployee(int id, string firstName, string lastName, bool isActive, Date bornDate, Date hireDay, decimal salary) : 
+        base(id, firstName, lastName, isActive, bornDate, hireDay)
+    {
+        Salary = salary;
+    }
+
+    //properties
+    public decimal Salary 
+    { 
+        get => _salary; 
+        set => _salary = ValidateSalary(value); 
+    }
+
+    //methods
+    //public methods
+    public override decimal GetValueToPay() => Salary; //arrow notation, same as return and it does the process in just one line
+
+    public override string ToString() => base.ToString();
+
+    //private methods
+    private decimal ValidateSalary(decimal salary)
+    {
+        if (salary < 2000000)
+        {
+            throw new ArgumentOutOfRangeException(nameof(salary), "Salary must be greater than or equal to $2,000,000.");
+        }
+        return salary;
+    }
+
+}
--- ConceptsOfOOP/OPPConcepts.Frontend/Program.cs
+++ ConceptsOfOOP/OPPConcepts.Frontend/Program.cs
@@ -20,9 +20,12 @@ catch (Exception ex)
 
 try
 {
-
+    var employee1 = new SalaryEmployee(1010, "Maria", "Perez", true, new Date(1990, 5, 15), new Date(2020, 1, 1), 2500000);
+    var employee2 = new SalaryEmployee(2020, "Joaquin", "Gonzalez", true, new Date(1970, 4, 12), new Date(2015, 11, 16), 10395876);
+    Console.WriteLine(employee1);
+    Console.WriteLine(employee2);
 } 
-catch
+catch (Exception ex)
 {
-
+    Console.WriteLine(ex.Message);
 }
